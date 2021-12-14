1b5bbe3386b0f6b6d882bbce9d83c3a3e82a6f42
kim-dongho <dongho_k@naver.com>
2021-12-15 03:29:53 +0900

    version 2.1

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import './Loading.css';
 const Loading = () => {
   return (
     <div className='loader'>
-      <Loader type='TailSpin' color='#00BFFF' height={100} width={100} />
+      <Loader type='TailSpin' color='#00BFFF' height={120} width={120} />
       <span>Loading...</span>
     </div>
   );
